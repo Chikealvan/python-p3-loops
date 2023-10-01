4930f825d456720d28b70239daaeafafd693274a
chikealvan <nwadoziechikealvan@gmail.com>
2023-10-01 14:28:21 +0100

    first commit

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,24 @@
 
 def happy_new_year():
     # code goes here!
+    for countdown in range(10, 0, -1):
+        print(countdown)
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
 
-def fizzbuzz():
-    # code goes here!
-    pass
+def fizzbuzz(input_string):
+    reversed_string = ""
+    for char in input_string:
+        reversed_string = char + reversed_string
+    return reversed_string
